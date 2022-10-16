f1072f5419c6b4a12248d8c6c1e381f697e4e4e3
Dwahgon <therealdragonofwar@gmail.com>
2022-10-16 17:53:52 -0300

    fix: load animal texture properly

--- Animais/Animal.gd
+++ Animais/Animal.gd
@@ -3,11 +3,7 @@ extends Node2D
 export(int) var id_animal: int = 0
 
 func _ready():
-	print()
-	var texture = ImageTexture.new()
-	var image = Image.new()
-	image.load("res://Assets/Sprites/"+Globais.dados_animais[str(id_animal)]["nome"]+".png")
-	texture.create_from_image(image)
+	var texture: StreamTexture = load("res://Assets/Sprites/"+Globais.dados_animais[str(id_animal)]["nome"]+".png")
 	$Sprite.texture = texture
 
 
