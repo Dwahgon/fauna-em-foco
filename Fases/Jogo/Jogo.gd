c321d215083a1085c72efc2e4b029ed4064e9757
Dwahgon <therealdragonofwar@gmail.com>
2022-10-15 22:46:43 -0300

    Mudar nome da foto

--- Fases/Jogo/Jogo.gd
+++ Fases/Jogo/Jogo.gd
@@ -1,12 +1,13 @@
 extends Node
 
 const CENA_POLAROID = preload("res://Polaroid/Polaroid.tscn")
+const NOME_FOTO = "imagem%d"
 
 export (int) var quantidade_fitas := 10
 
 onready var maquina_fotografica = $ViewportJogo/MundoJogo/MaquinaFotografica
 
-var fitas_restantes
+var fitas_restantes: int
 
 var _fotos_tiradas = []
 
@@ -16,10 +17,13 @@ func _ready():
 	fitas_restantes = quantidade_fitas
 	emit_signal("fitas_restantes_atualizada", fitas_restantes)
 	_fotos_tiradas.clear()
+	_atualizar_nome_foto()
 
 
 func _on_MaquinaFotogrfica_foto_tirada(nome_imagem, _objetos_na_foto):
-	fitas_restantes -= 1
+	fitas_restantes = int(clamp(fitas_restantes - 1, 0, fitas_restantes))
+	
+	_atualizar_nome_foto()
 	emit_signal("fitas_restantes_atualizada", fitas_restantes)
 	
 	_adicionar_foto(nome_imagem, _objetos_na_foto)
@@ -27,6 +31,10 @@ func _on_MaquinaFotogrfica_foto_tirada(nome_imagem, _objetos_na_foto):
 	_criar_polaroid(maquina_fotografica.position, nome_imagem)
 	if fitas_restantes == 0:
 		print("Jogo acabou!")
+
+
+func _atualizar_nome_foto():
+	maquina_fotografica.nome_foto = NOME_FOTO % (quantidade_fitas-fitas_restantes)
 
 
 func _criar_polaroid(posicao, nome_imagem):
