9f70fac873bed38d5cc6268b469ab5e4578dc77e
Dwahgon <therealdragonofwar@gmail.com>
2022-10-17 01:37:18 -0300

    Finalizar

--- Interface/MenuInicial/Fade.gd
+++ Interface/MenuInicial/Fade.gd
@@ -6,6 +6,10 @@ func reproduzir_animacao():
 	$AnimationPlayer.play("fade_in")
 
 
+func reproduzir_fadeout():
+	$AnimationPlayer.play("fade_out")
+
+
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "fade_in":
 		emit_signal("fade_in_finalizado")
